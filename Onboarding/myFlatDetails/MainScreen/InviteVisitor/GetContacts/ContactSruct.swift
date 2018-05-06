6265d9366672911b6affd68581f54ee9d4f26804
Vikas Nayak <thevikasnayak@gmail.com>
2018-05-06 19:37:30 +0530

    designed invite visitor page

---
+++ Onboarding/myFlatDetails/MainScreen/InviteVisitor/GetContacts/ContactSruct.swift
@@ -0,0 +1,9 @@
+//
+//  ContactSruct.swift
+//  nammaApartment
+//
+//  Created by Vikas Nayak on 05/05/18.
+//  Copyright © 2018 Vikas Nayak. All rights reserved.
+//
+
+import Foundation
---
+++ Onboarding/myFlatDetails/MainScreen/InviteVisitor/GetContacts/GetContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GetContactTableViewCell.swift
+//  nammaApartment
+//
+//  Created by Vikas Nayak on 05/05/18.
+//  Copyright © 2018 Vikas Nayak. All rights reserved.
+//
+
+import UIKit
+
+class GetContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
