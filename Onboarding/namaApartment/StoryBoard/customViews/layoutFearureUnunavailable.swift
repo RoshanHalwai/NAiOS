20c3023a9192c47ab2984d35b1cf2ef35bab6b2c
Vikas Nayak <thevikasnayak@gmail.com>
2018-06-27 12:56:26 +0530

    customView

---
+++ Onboarding/namaApartment/StoryBoard/customViews/layoutFearureUnunavailable.swift
@@ -0,0 +1,21 @@
+//
+//  layoutFearureUnunavailable.swift
+//  nammaApartment
+//
+//  Created by Vikas Nayak on 27/06/18.
+//  Copyright © 2018 Vikas Nayak. All rights reserved.
+//
+
+import UIKit
+
+class layoutFearureUnunavailable: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
