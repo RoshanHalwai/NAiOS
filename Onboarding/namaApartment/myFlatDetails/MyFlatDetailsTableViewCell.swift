f4bd912499ade188bca8e7f7cb014aafeddb07fe
Sundir Talari <sundirtalari@Sundirs-MacBook-Air.local>
2018-06-29 16:44:36 +0530

    Code Refactoring

--- Onboarding/namaApartment/myFlatDetails/MyFlatDetailsTableViewCell.swift
+++ Onboarding/namaApartment/myFlatDetails/MyFlatDetailsTableViewCell.swift
@@ -13,10 +13,6 @@ class MyFlatDetailsTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
         list_Label.font = NAFont().descriptionFont()
-    }
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
     }
 }
