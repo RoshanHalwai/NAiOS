5230b5175c7d334f2a3ab2ed9bdc1d8a8874b328
kalpana <kalrohi@kalpanas-MacBook-Pro.local>
2018-07-02 17:21:39 +0530

    code refactoring

--- Onboarding/namaApartment/MainScreen/SocietyServices/DigitalGate/InviteVisitor/InviteVisitorViewController.swift
+++ Onboarding/namaApartment/MainScreen/SocietyServices/DigitalGate/InviteVisitor/InviteVisitorViewController.swift
@@ -116,7 +116,7 @@ class InviteVisitorViewController: NANavigationViewController,CNContactPickerDel
         lbl_Date.font = NAFont().headerFont()
         lbl_InvitorName.text = NAString().visitorName()
         lbl_InvitorMobile.text = NAString().visitorMobile()
-//        lbl_InviteDescription.text = NAString().inviteVisitorOTPDesc()
+        lbl_InviteDescription.text = NAString().inviteVisitorOTPDesc()
         
         lbl_Name_Validation.font = NAFont().descriptionFont()
         lbl_Mob_Validation.font = NAFont().descriptionFont()
