1af7aba2edc820d14e23a00fa0e7e1c40ad96efd
kalpana <kalrohi@kalpanas-MacBook-Pro.local>
2018-07-18 10:42:09 +0530

    ScreenChange

--- Onboarding/namaApartment/MainScreen/SocietyServices/DigitalGate/MyVisitorList/VisitorListCategory/CabAndPackageArrivalCardListViewController.swift
+++ Onboarding/namaApartment/MainScreen/SocietyServices/DigitalGate/MyVisitorList/VisitorListCategory/CabAndPackageArrivalCardListViewController.swift
@@ -18,29 +18,38 @@ class CabAndPackageArrivalCardListViewController: NANavigationViewController, UI
     var vendorCabNameString: String?
     var vendorCabImage: UIImage?
     
+    //Creating  Object with Boolean Value false to know from where the screen is coming from.
+    var fromExpectingArrivalVC = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Formatting & setting navigation bar
+        /* * Formatting & setting navigation bar
+           * Calling Change Label title Function
+           * Created custom back button for navigating back to Digi Gate VC. */
+        
         super.ConfigureNavBarTitle(title: navTitle)
         self.navigationItem.title = ""
         
-        //Calling Change Label title Function
         changeLblTitle()
         
-        //created custom back button for navigating back to My Visitor List VC
-        let backButton = UIBarButtonItem(image: #imageLiteral(resourceName: "backk24"), style: .plain, target: self, action: #selector(goBackToMyVisitorListVC))
-        self.navigationItem.leftBarButtonItem = backButton
         self.navigationItem.hidesBackButton = true
+        let backButton = UIBarButtonItem(image: #imageLiteral(resourceName: "backk24"), style: .plain, target: self, action: #selector(goBackToDigiGateVC))
+        self.navigationItem.leftBarButtonItem = backButton
     }
     
-    //to navigate back to My Visitor List VC
-    @objc func goBackToMyVisitorListVC() {
-        let dv = NAViewPresenter().myVisitorsListVC()
-        self.navigationController?.pushViewController(dv, animated: true)
+    /* * To navigate back to Digi Gate VC
+       * Changing label titles in based on Navigation title. */
+    
+    @objc func goBackToDigiGateVC() {
+        if fromExpectingArrivalVC {
+        let vcToPop = self.navigationController?.viewControllers[(self.navigationController?.viewControllers.count)!-4]
+            self.navigationController?.popToViewController(vcToPop!, animated: true)
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
     }
     
-    //Changing label titles in based on Navigation title
     func changeLblTitle() {
         if navTitle == NAString().cab_arrival() {
             vendorCabNameString = NAString().cab_no()
@@ -55,10 +64,15 @@ class CabAndPackageArrivalCardListViewController: NANavigationViewController, UI
         return 1
     }
     
+    /* * This creates the shadows and modifies the cards a little bit
+       * Assigning font & style to cell labels
+       * Setting image round
+       * Assigning Strings to the label tile according to Navigation Title
+       * Assigning Images to the Image View according to Navigation Title. */
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NAString().cellID(), for: indexPath) as! CabAndPackageArrivalCardListCollectionViewCell
         
-        //This creates the shadows and modifies the cards a little bit
         cell.contentView.layer.cornerRadius = 4.0
         cell.contentView.layer.borderWidth = 1.0
         cell.contentView.layer.borderColor = UIColor.clear.cgColor
@@ -70,27 +84,22 @@ class CabAndPackageArrivalCardListViewController: NANavigationViewController, UI
         cell.layer.masksToBounds = false
         cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
         
-        //assigning font & style to cell labels
         cell.lbl_CabNo_Type.font = NAFont().textFieldFont()
         cell.lbl_Date_Type.font = NAFont().textFieldFont()
         cell.lbl_InTime_Type.font = NAFont().textFieldFont()
         cell.lbl_Status_Type.font = NAFont().textFieldFont()
         cell.lbl_Inviter_Type.font = NAFont().textFieldFont()
-        
         cell.lbl_CabNo_Detail.font = NAFont().headerFont()
         cell.lbl_Date_Detail.font = NAFont().headerFont()
         cell.lbl_InTime_Detail.font = NAFont().headerFont()
         cell.lbl_Status_Detail.font = NAFont().headerFont()
         cell.lbl_Inviter_Detail.font = NAFont().headerFont()
         
-        //setting image round
         cell.image_View.layer.cornerRadius = cell.image_View.frame.size.width/2
         cell.image_View.clipsToBounds = true
         
-        //Assigning Strings to the label tile according to Navigation Title
         cell.lbl_CabNo_Type.text = vendorCabNameString
         
-        //Assigning Images to the Image View according to Navigation Title
         cell.image_View.image = vendorCabImage
         return cell
     }
