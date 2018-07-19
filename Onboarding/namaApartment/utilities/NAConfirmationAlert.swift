08179d1dbcc137be9bfcf0d37677905f928e2ed6
Sundir Talari <sundirtalari@Sundirs-MacBook-Air.local>
2018-07-19 15:07:46 +0530

    Code Refactoring

--- Onboarding/namaApartment/utilities/NAConfirmationAlert.swift
+++ Onboarding/namaApartment/utilities/NAConfirmationAlert.swift
@@ -13,8 +13,8 @@ class NAConfirmationAlert: NSObject {
     
     func showConfirmationDialog(VC: UIViewController, Title: String, Message: String, CancelStyle: UIAlertActionStyle, OkStyle: UIAlertActionStyle, OK: ((UIAlertAction) -> Void)?, Cancel: ((UIAlertAction) -> Void)?) {
         let alert = UIAlertController(title: Title , message: Message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: OK))
-        alert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: Cancel))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: Cancel))
+        alert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: OK))
         alert.view.backgroundColor = UIColor.white
         alert.view.layer.cornerRadius = 10
         VC.present(alert, animated: true)
