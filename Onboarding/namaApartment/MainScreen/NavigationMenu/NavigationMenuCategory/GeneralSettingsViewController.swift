9f11b7d565ffdf0288e5142c093f75d732c3e5ac
kalpana <kalrohi@kalpanas-MacBook-Pro.local>
2018-07-25 19:12:41 +0530

    NavigationLaguages

--- Onboarding/namaApartment/MainScreen/NavigationMenu/NavigationMenuCategory/GeneralSettingsViewController.swift
+++ Onboarding/namaApartment/MainScreen/NavigationMenu/NavigationMenuCategory/GeneralSettingsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class GeneralSettingsViewController: NANavigationViewController {
+class GeneralSettingsViewController: NANavigationViewController,UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet weak var choose_Language_Label: UILabel!
     @IBOutlet weak var location_Services_Label: UILabel!
@@ -16,14 +16,22 @@ class GeneralSettingsViewController: NANavigationViewController {
     @IBOutlet weak var app_Version_Label: UILabel!
     @IBOutlet weak var report_Bug_TextField: UITextField!
     @IBOutlet weak var location_Services_Switch: UISwitch!
+    @IBOutlet weak var language_Btn: UIButton?
+    @IBOutlet weak var language_View: UIView?
+    @IBOutlet weak var tableView: UITableView?
     
     var navTitle = String()
+    let languagesList = ["English","Hindi","Tamil","Kannada","Telugu"]
     
-    /* - Assigning Font and line for text field.
-       - Assigning text and Font to Labels. */
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /* - Assigning Font and line for text field.
+         - Assigning text and Font to Labels. */
+        
+        language_View?.frame = CGRect(x: 15, y: 50, width: 300, height: 250)
+        self.view.addSubview(self.language_View!)
+        language_View?.isHidden = true
         
         super.ConfigureNavBarTitle(title: navTitle)
         
@@ -39,6 +47,44 @@ class GeneralSettingsViewController: NANavigationViewController {
         location_Services_Label.font = NAFont().headerFont()
         report_A_Bug_Label.font = NAFont().headerFont()
         app_Version_Label.font = NAFont().headerFont()
+        language_Btn?.titleLabel?.font = NAFont().headerFont()
     }
-
+    
+    /* - Calling TableView Data Source & Delegate Methods & UIView Methods.
+     - Create Language Button Action. */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return languagesList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NAString().cellID(), for: indexPath)
+        cell.textLabel?.text = languagesList[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        /* - Getting the index path of selected row.
+         - Getting the current cell from the index path.
+         - Getting the text of that cell. */
+        
+        let indexPath = tableView.indexPathForSelectedRow
+        
+        let currentCell = tableView.cellForRow(at: indexPath!)! as UITableViewCell
+        let language = (currentCell.textLabel?.text)!
+        language_Btn?.titleLabel?.text = language
+        language_View?.isHidden = true
+    }
+    
+    @IBAction func btn_LanguageAction() {
+        language_View?.isHidden = false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        language_View?.isHidden = true
+    }
 }
