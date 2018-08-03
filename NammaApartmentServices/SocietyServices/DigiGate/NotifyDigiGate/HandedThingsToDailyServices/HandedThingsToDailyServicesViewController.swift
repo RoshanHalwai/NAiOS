3404bdcfcd958928bcf192934c0b2b8f78386cbf
Vikas Nayak <thevikasnayak@gmail.com>
2018-08-03 19:51:29 +0530

    Implemented Auto Scrolling if keyboard will appear in HandedThinsg To DS.

--- NammaApartmentServices/SocietyServices/DigiGate/NotifyDigiGate/HandedThingsToDailyServices/HandedThingsToDailyServicesViewController.swift
+++ NammaApartmentServices/SocietyServices/DigiGate/NotifyDigiGate/HandedThingsToDailyServices/HandedThingsToDailyServicesViewController.swift
@@ -34,6 +34,10 @@ class HandedThingsToDailyServicesViewController: NANavigationViewController, UIT
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //TableView cell move up automatically, If keyboard will appaer
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
         //Calling Handed Things TO Daily services Retrieving Function on Load
         retrieveHandedThingsDSFirebase()
         
@@ -58,6 +62,19 @@ class HandedThingsToDailyServicesViewController: NANavigationViewController, UIT
         //Formatting & setting navigation bar
         super.ConfigureNavBarTitle(title: titleName)
         self.navigationItem.title = ""
+    }
+    
+    //TableView cell move up automatically, If when keyboard will appaer
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            tableView.contentInset = UIEdgeInsetsMake(0, 0, keyboardSize.height, 0)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        if ((notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+            tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        }
     }
     
     //to Navigate to Daily Services History VC
