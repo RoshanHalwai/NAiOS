e09955fe602d0e3f3e9a7eacf8f48e8273425706
Vikas Nayak <thevikasnayak@gmail.com>
2018-08-29 17:37:48 +0530

    Resolved daily Services status issue while storing

--- NammaApartmentServices/SocietyServices/DigiGate/New Group/AddMyServices/AddMyServicesViewController.swift
+++ NammaApartmentServices/SocietyServices/DigiGate/New Group/AddMyServices/AddMyServicesViewController.swift
@@ -482,7 +482,12 @@ extension AddMyServicesViewController {
         
         dailyServicesStatusRef = Database.database().reference().child(Constants.FIREBASE_CHILD_DAILY_SERVICES).child(Constants.FIREBASE_USER_CHILD_ALL).child(Constants.FIREBASE_USER_PUBLIC).child(dailyServiceKey).child(dailyServiceUID!)
         
-        self.dailyServicesStatusRef?.child(NAString().status()).setValue(NAString().notEntered())
+        dailyServicesStatusRef?.observeSingleEvent(of: .value, with: { (dsAvailableSnapshot) in
+            
+            if !dsAvailableSnapshot.exists() {
+                self.dailyServicesStatusRef?.child(NAString().status()).setValue(NAString().notEntered())
+            }
+        })
         
         dailyServicesImageRef = Storage.storage().reference().child(Constants.FIREBASE_CHILD_DAILY_SERVICES).child(Constants.FIREBASE_CHILD_PRIVATE).child(dailyServiceKey).child(Constants.FIREBASE_CHILD_OWNERSUID).child(userUID)
         
