4e6be6db0d329fc4de712dce5d64f5e862d1521c
kalpana <kalrohi@kalpanas-MacBook-Pro.local>
2018-08-30 09:48:53 +0530

    FixingTransparentView

--- Utilities/OpacityView.swift
+++ Utilities/OpacityView.swift
@@ -21,15 +21,19 @@ class OpacityView {
     
     //Create showing Opacity view
     func showingOpacityView(view: UIViewController) {
-        opacityView.frame = CGRect(x: 0, y:0, width:view.view.frame.width, height: view.view.frame.height)
-        opacityView.backgroundColor = UIColor.black
-        opacityView.alpha = 0.5
+        // get your window screen size
+        let screenSize = UIScreen.main.bounds
+        //create a new view with the same size
+        opacityView = UIView(frame: screenSize)
+        // change the background color to black and the opacity to 0.6
+        opacityView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        // add this new view to your main view
         view.view.addSubview(opacityView)
     }
     
     //Create showing popup view
     func showingPopupView(view: UIViewController) {
-        popupView = PopupView(frame: CGRect(x: 0, y:0, width: 250, height: 150))
+        popupView = PopupView(frame: CGRect(x: 0, y:0, width: 280, height: 150))
         popupView.center.x = view.view.bounds.width/2
         popupView.center.y = view.view.bounds.height/2
         popupView.lbl_Message.text = NAString().addButtonloadViewMessage()
