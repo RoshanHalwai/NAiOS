4dfd05c43a21a27fbb634dc9af4ed161b28137a3
Vikas Nayak <thevikasnayak@gmail.com>
2018-09-04 19:46:45 +0530

    Fixed MyGuard crashing issue

--- NavigationDrawer/NavigationMenuCategory/My Guards/MyGuardsCollectionViewCell.swift
+++ NavigationDrawer/NavigationMenuCategory/My Guards/MyGuardsCollectionViewCell.swift
@@ -14,7 +14,7 @@ class MyGuardsCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var lbl_MyGuardName: UILabel!
     @IBOutlet weak var lbl_MyGuardGateNo: UILabel!
     @IBOutlet weak var lbl_MyGuardStatus: UILabel!
-    
+    @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var lbl_GuardName: UILabel!
     @IBOutlet weak var lbl_GuardGateNo: UILabel!
     @IBOutlet weak var lbl_GuardStatus: UILabel!
