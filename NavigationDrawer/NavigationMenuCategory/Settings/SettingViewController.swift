95a86ba9630ee5b63f0d3e3dcdee2557b966e8c7
Vikas Nayak <thevikasnayak@gmail.com>
2018-09-05 14:57:46 +0530

    Retrieving notification sound values from firebase to make UI switches

--- NavigationDrawer/NavigationMenuCategory/Settings/SettingViewController.swift
+++ NavigationDrawer/NavigationMenuCategory/Settings/SettingViewController.swift
@@ -85,11 +85,11 @@ class SettingViewController: NANavigationViewController {
     
     @IBAction func switch_EIntercom(_ sender: UISwitch) {
         if (sender.isOn ==  true) {
-           userNotificationRef?.child(Constants.FIREBASE_CHILD_EINTERCOM_SOUND).setValue(NAString().gettrue())
+            userNotificationRef?.child(Constants.FIREBASE_CHILD_EINTERCOM_SOUND).setValue(NAString().gettrue())
         } else {
-             userNotificationRef?.child(Constants.FIREBASE_CHILD_EINTERCOM_SOUND).setValue(NAString().getfalse())
+            userNotificationRef?.child(Constants.FIREBASE_CHILD_EINTERCOM_SOUND).setValue(NAString().getfalse())
         }
-     }
+    }
     
     @IBAction func switch_Guest(_ sender: UISwitch) {
         if (sender.isOn ==  true) {
@@ -168,7 +168,6 @@ extension SettingViewController {
             .child(Constants.FIREBASE_CHILD_NOTIFICATION_SOUND)
         
         notificationSoundRef.observeSingleEvent(of: .value) { (soundSnapshot) in
-            print(soundSnapshot as Any)
             let notificationSoundData = soundSnapshot.value as! [String: Any]
             
             let cabValue = notificationSoundData[Constants.FIREBASE_CHILD_CAB_SOUND] as! Bool
@@ -177,19 +176,38 @@ extension SettingViewController {
             let guestValue = notificationSoundData[Constants.FIREBASE_CHILD_GUEST_SOUND] as! Bool
             let eIntercomValue = notificationSoundData[Constants.FIREBASE_CHILD_EINTERCOM_SOUND] as! Bool
             
-            print(cabValue as Any)
-            print(PackageValue as Any)
-            print(dailyServiceValue as Any)
-            print(guestValue as Any)
-            print(eIntercomValue as Any)
-            
-            if cabValue == false {
-                self.switch_Cab.isOn = false
+            //Setting UISwitches isOn accroding to firebase values
+            if cabValue == true {
+                self.switch_Cab.isOn = true
             } else {
-                     self.switch_Cab.isOn = true
-                }
+                self.switch_Cab.isOn = false
+            }
+            
+            if PackageValue == true {
+                self.switch_Package.isOn = true
+            } else {
+                self.switch_Package.isOn = false
+            }
+            
+            if dailyServiceValue == true {
+                self.switch_DailyServices.isOn = true
+            } else {
+                self.switch_DailyServices.isOn = false
+            }
+            
+            if guestValue == true {
+                self.switch_Guest.isOn = true
+            } else {
+                self.switch_Guest.isOn = false
+            }
+            
+            if eIntercomValue == true {
+                self.switch_EIntercom.isOn = true
+            } else {
+                self.switch_EIntercom.isOn = false
             }
         }
     }
+}
 
 
