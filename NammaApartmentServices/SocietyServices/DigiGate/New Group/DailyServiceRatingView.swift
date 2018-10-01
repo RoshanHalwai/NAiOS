99ca7ea7cd91dbae6d6fc9afc9a4960cadf39457
kirtan labs <kirtanlabs@kirtans-MacBook-Air.local>
2018-10-01 13:14:27 +0530

    Code Refactoring

---
+++ NammaApartmentServices/SocietyServices/DigiGate/New Group/DailyServiceRatingView.swift
@@ -0,0 +1,9 @@
+//
+//  DailyServiceRatingView.swift
+//  nammaApartment
+//
+//  Created by kirtan labs on 01/10/18.
+//  Copyright © 2018 Vikas Nayak. All rights reserved.
+//
+
+import Foundation
