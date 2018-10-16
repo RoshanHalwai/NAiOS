b317b143c3e5da0292326f1accf8fe5799397261
kirtan labs <kirtanlabs@kirtans-MacBook-Air.local>
2018-10-16 19:21:30 +0530

    Code Refactoring

--- NavigationDrawer/NavigationMenuCategory/New Group/SendMessageViewController.swift
+++ NavigationDrawer/NavigationMenuCategory/New Group/SendMessageViewController.swift
@@ -13,6 +13,10 @@ class SendMessageViewController: NANavigationViewController, UITableViewDataSour
     @IBOutlet weak var text_View: UITextView!
     @IBOutlet weak var btn_Send: UIButton!
     @IBOutlet weak var table_View: UITableView!
+    @IBOutlet weak var stackView_Bottom_Constraints: NSLayoutConstraint!
+    @IBOutlet weak var text_View_Height_Constraints: NSLayoutConstraint!
+    
+    @IBOutlet weak var table_View_Bottom_Constraint: NSLayoutConstraint!
     
     var neighbourUID = String()
     var neighbourApartment = String()
@@ -34,14 +38,15 @@ class SendMessageViewController: NANavigationViewController, UITableViewDataSour
         text_View.font = NAFont().textFieldFont()
         retrieveNeighboursMessages()
         table_View.separatorStyle = .none
+       
         
         
         table_View.estimatedRowHeight = 100
         
         table_View.rowHeight = UITableViewAutomaticDimension
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil);
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil);
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapFunction))
         view.isUserInteractionEnabled = true
@@ -54,13 +59,35 @@ class SendMessageViewController: NANavigationViewController, UITableViewDataSour
         view.endEditing(true)
     }
     
-    @objc func keyboardWillShow(sender: NSNotification) {
-        if self.view.frame.origin.y >= 0 {
-            self.view.frame.origin.y -= 255
+    @objc func keyBoardWillShow(notification: Notification){
+        if let userInfo = notification.userInfo as? Dictionary<String, AnyObject>{
+            let frame = userInfo[UIKeyboardFrameEndUserInfoKey]
+            let keyBoardRect = frame?.cgRectValue
+            if let keyBoardHeight = keyBoardRect?.height {
+                self.stackView_Bottom_Constraints.constant = keyBoardHeight
+                //self.table_View_Bottom_Constraint.constant = keyBoardHeight
+                
+                
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.view.layoutIfNeeded()
+                })
+            }
         }
     }
-    @objc func keyboardWillHide(sender: NSNotification) {
-        self.view.frame.origin.y += 255
+    
+    @objc func keyBoardWillHide(notification: Notification){
+        if let userInfo = notification.userInfo as? Dictionary<String, AnyObject>{
+            let frame = userInfo[UIKeyboardFrameEndUserInfoKey]
+            let keyBoardRect = frame?.cgRectValue
+            if let keyBoardHeight = keyBoardRect?.height {
+                self.stackView_Bottom_Constraints.constant = self.stackView_Bottom_Constraints.constant - keyBoardHeight
+                //self.table_View_Bottom_Constraint.constant = self.table_View_Bottom_Constraint.constant - keyBoardHeight
+               
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.view.layoutIfNeeded()
+                })
+            }
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -115,6 +142,19 @@ class SendMessageViewController: NANavigationViewController, UITableViewDataSour
         }
     }
     
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        let numberOfLines: Int = Int(self.text_View.contentSize.height/(self.text_View.font?.lineHeight)!)
+        let estimatedSize = self.text_View.contentSize.height
+        text_View.constraints.forEach { (constraints) in
+            self.text_View_Height_Constraints.constant = estimatedSize
+        }
+        if numberOfLines <= 5 {
+            self.text_View_Height_Constraints.constant = self.text_View.contentSize.height
+        } 
+        return true
+    }
+    
     func storeMessageInFirebase() {
         let userDataRef = GlobalUserData.shared.getUserDataReference()
             .child(Constants.FIREBASE_CHILD_CHATS)
@@ -142,6 +182,11 @@ class SendMessageViewController: NANavigationViewController, UITableViewDataSour
                     .child(userUID)
                 neighbourDataRef.setValue(chatRoomUID)
             }
+            let chatAllRef = Constants.FIREBASE_DATABASE_REFERENCE
+                .child(Constants.FIREBASE_CHILD_CHATS).child(Constants.FIREBASE_USER_CHILD_ALL).child(chatRoomUID!)
+            let UIDs = [userUID: NAString().gettrue(),
+                        self.neighbourUID: NAString().gettrue()]
+            chatAllRef.setValue(UIDs)
             
             let chatRef = Constants.FIREBASE_DATABASE_REFERENCE
                 .child(Constants.FIREBASE_CHILD_CHATS)
