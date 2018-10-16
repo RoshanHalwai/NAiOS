3fea7f459d77658b4bf3519a68a749e99b41705d
kalpana <kalrohi@kalpanas-MacBook-Pro.local>
2018-10-16 16:19:27 +0530

    Code refactoring

--- NavigationDrawer/NavigationMenuCategory/My Profile/EditMyProfileFlatMembersListViewController.swift
+++ NavigationDrawer/NavigationMenuCategory/My Profile/EditMyProfileFlatMembersListViewController.swift
@@ -97,9 +97,51 @@ class EditMyProfileFlatMembersListViewController: NANavigationViewController, UI
         let indexPath = tableView.indexPathForSelectedRow
         let currentCell = tableView.cellForRow(at: indexPath!)! as UITableViewCell;()
         let currentItem = currentCell.textLabel?.text
-        myProfileVC.updatedAdminUID = myProfileVC.allFlatMembersUID[(indexPath?.row)!]
-        myProfileVC.selectedMember = currentItem!
+        
+        let updatedAdminUID = myProfileVC.allFlatMembersUID[(indexPath?.row)!]
+        
+        // Showing Confirmation Alert PopUp for changing Admin Access
+        NAConfirmationAlert().showConfirmationDialog(VC: self, Title: NAString().change_Admin_Message_Alert_Title(), Message: NAString().change_Admin_Message_Alert_Message(name: currentItem!), CancelStyle: .default, OkStyle: .default, OK: { (action) in
+            
+            //After Changing Admin Access Previous Admin user Admin Access changed to false.
+            let removedAdminRef =  Constants.FIREBASE_USERS_PRIVATE.child(self.myProfileVC.adminUID!).child(Constants.FIREBASE_CHILD_PRIVILEGES)
+            removedAdminRef.child(Constants.FIREBASE_CHILD_ADMIN).setValue(NAString().getfalse())
+            //Setting the admin Value in GlobalUser data after User Changed Admin Access
+            GlobalUserData.shared.privileges_Items.first?.setAdmin(admin: NAString().getfalse())
+            
+            //After Changing Admin Access new Admin UID will get Replaced with previous UID
+            let UpdatedUserDataAdminRef = GlobalUserData.shared.getUserDataReference()
+            UpdatedUserDataAdminRef.child(Constants.FIREBASE_CHILD_ADMIN).setValue(updatedAdminUID)
+            
+            //After Changing Admin Access new Admin user Admin Access will be Chnaged to true
+            let updatedUserAdminRef = Constants.FIREBASE_USERS_PRIVATE.child(updatedAdminUID).child(Constants.FIREBASE_CHILD_PRIVILEGES)
+            updatedUserAdminRef.child(Constants.FIREBASE_CHILD_ADMIN).setValue(NAString().gettrue())
+            
+            //After Changing Admin Access new Admin user Grant Access will be Changes to true
+            let updatedUserGrantAccessRef = Constants.FIREBASE_USERS_PRIVATE.child(updatedAdminUID).child(Constants.FIREBASE_CHILD_PRIVILEGES)
+            updatedUserGrantAccessRef.child(Constants.FIREBASE_CHILD_GRANTACCESS).setValue(NAString().gettrue())
+            
+            //Showing Successfully Updated PopUp after Data Replaced in firebase.
+            NAConfirmationAlert().showNotificationDialog(VC: self, Title: NAString().update_Alert_Title(), Message: NAString().update_Successfull_Alert_Message(), buttonTitle: NAString().ok(), OkStyle: .default) { (action) in
+                
+                //Making present user to logout from this App after Clicking ok in Alert PopUp.
+                self.logoutAction()
+            }
+        }, Cancel: { (action) in}, cancelActionTitle: NAString().no(), okActionTitle: NAString().yes())
         tableView.deselectRow(at: indexPath!, animated: true)
-        self.navigationController?.dismiss(animated: true)
+    }
+    
+    //To Logout the current user
+    func logoutAction() {
+        let preferences = UserDefaults.standard
+        let userUID = NAString().userDefault_USERUID()
+        let loggedIn = NAString().userDefault_Logged_In()
+        preferences.removeObject(forKey: userUID)
+        preferences.set(false, forKey: loggedIn)
+        preferences.synchronize()
+        if self.storyboard != nil {
+            let NavLogin = NAViewPresenter().loginVC()
+            self.navigationController?.pushViewController(NavLogin, animated: true)
+        }
     }
 }
